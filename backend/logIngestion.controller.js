f304bebc8dd508fdb6221f9e12ae559df01bdb2e
RavinakS <ravinasingh697@gmail.com>
2025-07-09 23:11:49 +0530

    add new log api controller is ready

--- backend/logIngestion.controller.js
+++ backend/logIngestion.controller.js
@@ -1,23 +1,27 @@
+const { addNewLog, getLogs } = require("./logIngestion.model");
+
 const addLogIngestion = async (req, res) => {
   try {
     const { level, message, resourceId, timestamp, traceId, commit, metadata } =
       req.body;
 
-    if (
-      !level ||
-      !message ||
-      !resourceId ||
-      !timestamp ||
-      !traceId ||
-      !commit ||
-      metadata
-    ) {
+    const requiredFieldsExist = checkRequiredFields(req.body);
+
+    if (!requiredFieldsExist) {
       return res.status(400).json({ error: "Missing required log fields" });
     }
 
-    const log = { timestamp, level, message };
-    logs.push(log);
+    const log = {
+      level,
+      message,
+      resourceId,
+      timestamp,
+      traceId,
+      commit,
+      metadata,
+    };
 
+    await addNewLog(log);
     res.status(201).json({ message: "Log ingested successfully", log });
   } catch (err) {
     console.log(err);
@@ -29,6 +33,26 @@ const getLogIngestions = async (req, res) => {
   } catch (err) {
     console.log(err);
   }
+};
+
+const checkRequiredFields = (log) => {
+  let requiredFieldsExists = true;
+  const { level, message, resourceId, timestamp, traceId, commit, metadata } =
+    log;
+
+  if (
+    !level ||
+    !message ||
+    !resourceId ||
+    !timestamp ||
+    !traceId ||
+    !commit ||
+    metadata
+  ) {
+    requiredFieldsExists = false;
+  }
+
+  return requiredFieldsExists;
 };
 
 module.exports = { addLogIngestion, getLogIngestions };
